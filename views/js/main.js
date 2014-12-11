0f7cdbe8aeb4b78029db2919fcbf6946d6687ecb
GabeKagan <gabekagan@live.com>
2014-12-11 13:53:27 -0500

    Don't actually know how to fix that yet
    However, reducing the amount of pizzas is potentially useful, too.

--- views/js/main.js
+++ views/js/main.js
@@ -526,13 +526,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   //These lines don't need to be in a for loop! They don't change based on the value of i. 
-  var elem = document.createElement('img'); 
-  elem.className = 'mover';
-  elem.src = "images/pizza.png";
-  elem.style.height = "100px";
-  elem.style.width = "73.333px";
+  //But if you take them out in this exact fashion, they don't get added to the scopes properly.
 
-  for (var i = 0; i < 200; i++) {
+
+  for (var i = 0; i < 60; i++) { //User would need an absolutely insane resolution to get 200 pizzas.
+    var elem = document.createElement('img'); 
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
